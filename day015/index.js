9ac9ce5048647469ec7e9fc0baf387fcddba572e
dadayama <dadayamism@gmail.com>
2021-02-21 13:27:55 +0900

    day15

---
+++ day015/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const $menuItems = document.querySelector('.bl_menu_items')
+  const $menuButton = document.querySelector('.bl_menu_button')
+  const $links = Array.from(document.querySelectorAll('.link'))
+
+  $links.forEach($link => {
+    $link.addEventListener('click', () => {
+      console.log($link)
+      $menuItems.classList.toggle('active')
+      $menuButton.classList.toggle('active')
+    })
+  })
+});
